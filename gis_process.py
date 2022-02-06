71d389ca1de095f92cd420a27fd47966abd04212
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-02-06 20:39:57 +0600

    major changes and exception handling.

--- gis_process.py
+++ gis_process.py
@@ -2,19 +2,28 @@ import geopandas as gpd
 from shapely.geometry import Polygon, LineString, Point
     
 def process(lat,longi):
-    fp="bgd_nhr_drought_barc/bgd_nhr_drought_sparsso.shp"
-    fp=open(fp)    
-    data=gpd.read_file(fp)
-    point=Point(longi,lat)
-    l=data.contains(point)
-    found=None
-    for i in range(0,len(l)):
-        if(l[i]==True):
-            found=i
-        break
+    
+    try:
+        fp="bgd_nhr_drought_barc/bgd_nhr_drought_sparsso.shp"
+        data=gpd.read_file(fp)
+        point=Point(longi,lat)
+        l=data.contains(point)
+        found=None
+        for i in range(0,len(l)):
+            if(l[i]==True):
+                found=i
+                break
 
-    thana_name=data.loc[i,'THANA_NAME']
-    drought_class=data.loc[i,'DROU_CLASS']
-    drought_class_name=data.loc[i,'DROUGHT_CL']
-    return f"Thana Name: {thana_name} \nClass: {drought_class} \nType: {drought_class_name}"
+        if found:
+            thana_name=data.loc[found,'THANA_NAME']
+            drought_class=data.loc[found,'DROU_CLASS']
+            drought_class_name=data.loc[found,'DROUGHT_CL']
+            return f"Thana Name: {thana_name} \nClass: {drought_class} \nType: {drought_class_name}"
+        else:
+            return "Coordinate Invalid"
+    
+    except Exception as e :
+        return f" "+e.__class__+" has occured.\n"
+
+    
 
